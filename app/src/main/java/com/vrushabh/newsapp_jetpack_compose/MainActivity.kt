8be1bf1b7fd85974ea746ea93f937cf2971e948d
Vrushabh <vrushabh@appsvit.com>
2025-02-22 02:29:00 +0530

    Onboarding Screens Implemented.

--- app/src/main/java/com/vrushabh/newsapp_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/vrushabh/newsapp_jetpack_compose/MainActivity.kt
@@ -4,22 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import com.vrushabh.newsapp_jetpack_compose.presentation.onboarding.OnBoardingScreen
 import com.vrushabh.newsapp_jetpack_compose.ui.theme.NewsApp_JetPack_composeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         installSplashScreen()
         setContent {
-            NewsApp_JetPack_composeTheme {
+            NewsApp_JetPack_composeTheme(
+                dynamicColor = false
+            ) {
+                Box(
+                    modifier = Modifier.background(MaterialTheme.colorScheme.background)
+                ) {
+                    OnBoardingScreen()
+                }
             }
         }
     }
--- app/src/main/java/com/vrushabh/newsapp_jetpack_compose/ui/theme/Theme.kt
+++ app/src/main/java/com/vrushabh/newsapp_jetpack_compose/ui/theme/Theme.kt
@@ -9,18 +9,25 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Blue,
+    background = Black,
+    error = DarkRed,
+    surface = LightBlack
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Blue,
+    background = Color.White,
+    error = LightRed,
+    surface = Color.White
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -48,6 +55,15 @@ fun NewsApp_JetPack_composeTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
     }
 
     MaterialTheme(
