fe464d8da2ca497abdd3c4d38139889a7fda5225
Vrushabh <vrushabh@appsvit.com>
2025-02-22 14:45:22 +0530

    Implemented the System Bar Colors.

--- app/src/main/java/com/vrushabh/newsapp_jetpack_compose/presentation/mainActivity/MainActivity.kt
+++ app/src/main/java/com/vrushabh/newsapp_jetpack_compose/presentation/mainActivity/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.vrushabh.newsapp_jetpack_compose.presentation.navgraph.NavGraph
 import com.vrushabh.newsapp_jetpack_compose.ui.theme.NewsApp_JetPack_composeTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,9 +31,17 @@ class MainActivity : ComponentActivity() {
             setKeepOnScreenCondition(condition = { viewModel.splashCondition.value })
         }
         setContent {
-            NewsApp_JetPack_composeTheme(
-                dynamicColor = false
-            ) {
+            NewsApp_JetPack_composeTheme(dynamicColor = false) {
+
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemUiColor = rememberSystemUiController()
+                SideEffect {
+                    systemUiColor.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
+
                 Box(
                     modifier = Modifier.background(MaterialTheme.colorScheme.background)
                 ) {
