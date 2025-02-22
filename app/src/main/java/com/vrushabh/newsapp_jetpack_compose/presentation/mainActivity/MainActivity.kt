8af26ad38340c8ca6774461e955b277fed0aa9e4
Vrushabh <vrushabh@appsvit.com>
2025-02-22 14:27:24 +0530

    Implemented the NavGraph.

--- app/src/main/java/com/vrushabh/newsapp_jetpack_compose/presentation/mainActivity/MainActivity.kt
+++ app/src/main/java/com/vrushabh/newsapp_jetpack_compose/presentation/mainActivity/MainActivity.kt
@@ -1,39 +1,31 @@
-package com.vrushabh.newsapp_jetpack_compose
+package com.vrushabh.newsapp_jetpack_compose.presentation.mainActivity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.lifecycleScope
-import com.vrushabh.newsapp_jetpack_compose.domain.usecases.app_entry.AppEntryUseCases
-import com.vrushabh.newsapp_jetpack_compose.presentation.onboarding.OnBoardingScreen
-import com.vrushabh.newsapp_jetpack_compose.presentation.onboarding.OnBoardingViewModel
+import com.vrushabh.newsapp_jetpack_compose.presentation.navgraph.NavGraph
 import com.vrushabh.newsapp_jetpack_compose.ui.theme.NewsApp_JetPack_composeTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        installSplashScreen()
+        installSplashScreen().apply {
+            setKeepOnScreenCondition(condition = { viewModel.splashCondition.value })
+        }
         setContent {
             NewsApp_JetPack_composeTheme(
                 dynamicColor = false
@@ -41,8 +33,7 @@ class MainActivity : ComponentActivity() {
                 Box(
                     modifier = Modifier.background(MaterialTheme.colorScheme.background)
                 ) {
-                    val viewModel: OnBoardingViewModel = hiltViewModel()
-                    OnBoardingScreen(onEvent = viewModel::onEvent)
+                    NavGraph(startDestination = viewModel.startDestination.value)
                 }
             }
         }
---
+++ app/src/main/java/com/vrushabh/newsapp_jetpack_compose/presentation/navgraph/NavGraph.kt
@@ -0,0 +1,49 @@
+package com.vrushabh.newsapp_jetpack_compose.presentation.navgraph
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
+import com.vrushabh.newsapp_jetpack_compose.presentation.onboarding.OnBoardingScreen
+import com.vrushabh.newsapp_jetpack_compose.presentation.onboarding.OnBoardingViewModel
+
+@Composable
+fun NavGraph(
+    startDestination: String
+) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = startDestination) {
+        navigation(
+            route = Route.AppStartNavigation.route,
+            startDestination = Route.OnBoardingScreen.route
+        ) {
+            composable(route = Route.OnBoardingScreen.route) {
+                val viewModel: OnBoardingViewModel = hiltViewModel()
+                OnBoardingScreen(
+                    onEvent = viewModel::onEvent
+                )
+            }
+        }
+
+        navigation(
+            route = Route.NewsNavigation.route,
+            startDestination = Route.HomeScreen.route
+        ) {
+            composable(route = Route.HomeScreen.route) {
+
+            }
+            composable(route = Route.SearchScreen.route) {
+
+            }
+            composable(route = Route.BookmarkScreen.route) {
+
+            }
+            composable(route = Route.DetailsScreen.route) {
+
+            }
+        }
+    }
+}
